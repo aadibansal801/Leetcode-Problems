f62bffc92bad4a3290d696630645e12e19e30c3f
aadibansal801 <aadibansal801@gmail.com>
2026-07-22 16:06:22 +0530

    Time: 47 ms (34.92%) | Memory: 48.3 MB (45.79%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
 
     const int INF = 1e9;
-    vector<vector<int>>dp;
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        dp.assign(n+1,vector<int>(amount+1,INF));
-        for(int i = 0; i<=n; i++){
-            dp[i][0] = 0;
-        }
+        vector<int>next(amount+1, INF);
+        next[0] = 0;
         for(int i = n-1; i>=0; i--){
+            vector<int>curr(amount+1, INF);
+            curr[0] = 0;
             for(int amnt = 1; amnt<=amount; amnt++){
-                int skip = dp[i+1][amnt];
+                int skip = next[amnt];
                 int take = INF;
-                if(coins[i]<=amnt) take = 1 + dp[i][amnt-coins[i]];
-                dp[i][amnt] = min(take, skip);
+                if(coins[i]<=amnt) take = 1 + curr[amnt-coins[i]];
+                curr[amnt] = min(take, skip);
             }
+            next = curr;
         }
-        int ans = dp[0][amount];
+        int ans = next[amount];
         if(ans >= INF) return -1;
         return ans;
     }
