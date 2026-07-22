031fcea3f8cad9f30960a3f766eedb0217e1deb3
aadibansal801 <aadibansal801@gmail.com>
2026-07-22 16:06:55 +0530

    Time: 27 ms (64.01%) | Memory: 17.8 MB (84.43%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -8,15 +8,12 @@ public:
         vector<int>next(amount+1, INF);
         next[0] = 0;
         for(int i = n-1; i>=0; i--){
-            vector<int>curr(amount+1, INF);
-            curr[0] = 0;
             for(int amnt = 1; amnt<=amount; amnt++){
                 int skip = next[amnt];
                 int take = INF;
-                if(coins[i]<=amnt) take = 1 + curr[amnt-coins[i]];
-                curr[amnt] = min(take, skip);
+                if(coins[i]<=amnt) take = 1 + next[amnt-coins[i]];
+                next[amnt] = min(take, skip);
             }
-            next = curr;
         }
         int ans = next[amount];
         if(ans >= INF) return -1;
