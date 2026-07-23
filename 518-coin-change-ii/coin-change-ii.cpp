0392327413147cec42ebbcd37bc75ca32e8163ea
aadibansal801 <aadibansal801@gmail.com>
2026-07-24 02:15:21 +0530

    Time: 19 ms (76.42%) | Memory: 10.4 MB (81.20%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
 
-    vector<vector<long long>>dp;
-
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<long long>prev(amount+1,0),curr(amount+1,0);
+        vector<long long>prev(amount+1,0);
         for(int amnt = 0; amnt<=amount; amnt++){
             prev[amnt] = (amnt % coins[0] == 0);
         }
@@ -14,12 +12,11 @@ public:
                 long long notTake = prev[amnt];
                 long long take = 0;
                 if(coins[i]<=amnt){
-                    take = curr[amnt-coins[i]];
+                    take = prev[amnt-coins[i]];
                 }
                 long long sum = take + notTake;
-                curr[amnt] = min(sum, (long long)2e9);
+                prev[amnt] = min(sum, (long long)2e9);
             }
-            prev = curr;
         }
         return (int)prev[amount];
     }
