76c71f515d141a9c42af1ddde7d272c5a6745216
aadibansal801 <aadibansal801@gmail.com>
2026-07-24 12:38:50 +0530

    Time: 2 ms (21.81%) | Memory: 8.7 MB (64.87%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -3,23 +3,23 @@ public:
 
     vector<int>dp;
 
-    int solve(int i, string& s){
-        if(i == s.size()) return 1;
-        if(s[i] == '0') return 0;
-        if(dp[i]!=-1) return dp[i];
-        int ways = solve(i+1,s);
-        if(i+1<s.size()){
-            int num = (s[i] - '0') * 10 + (s[i+1] - '0');
-            if(num>=10 && num<=26){
-                ways+=solve(i+2, s);
-            }
-        }
-        return dp[i] = ways;
-    }
-
     int numDecodings(string s) {
         int n = s.size();
-        dp.assign(n+1,-1);
-        return solve(0, s);
+        dp.assign(n+1,0);
+        dp[n] = 1;
+        for(int i = n-1; i>=0; i--){
+            if(s[i] == '0'){
+                dp[i] = 0;
+                continue;
+            }
+            dp[i] = dp[i+1];
+            if(i+1 < n){
+                int num = (s[i] - '0')*10 + (s[i+1] - '0');
+                if(num>=10 && num<=26){
+                    dp[i]+=dp[i+2];
+                }
+            }
+        }
+        return dp[0];
     }
 };
