d84c76afe677ac8222c0ff36e04c744e0127ee35
aadibansal801 <aadibansal801@gmail.com>
2026-07-25 23:18:27 +0530

    Time: 4 ms (32.08%) | Memory: 13.9 MB (46.61%) - LeetSync

--- 1242-matrix-block-sum/matrix-block-sum.cpp
+++ 1242-matrix-block-sum/matrix-block-sum.cpp
@@ -3,16 +3,21 @@ public:
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
         int m = mat.size();
         int n = mat[0].size();
+        vector<vector<int>>pref(m+1,vector<int>(n+1));
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=n; j++){
+                pref[i][j] = mat[i-1][j-1] + pref[i-1][j] + pref[i][j-1] - pref[i-1][j-1];
+            }
+        }
         vector<vector<int>>ans(m,vector<int>(n));
         for(int i = 0; i<m; i++){
             for(int j = 0; j<n; j++){
-                int sum = 0;
-                for(int r = max(0,i-k); r<=min(m-1,i+k); r++){
-                    for(int c = max(0,j-k); c<=min(n-1,j+k); c++){
-                        sum+=mat[r][c];
-                    }
-                }
-                ans[i][j] = sum;
+                int r1 = max(0,i-k);
+                int c1 = max(0,j-k);
+                int r2 = min(m-1,i+k);
+                int c2 = min(n-1,j+k);
+                r1++,c1++,r2++,c2++;
+                ans[i][j] = pref[r2][c2] - pref[r1-1][c2] - pref[r2][c1-1] + pref[r1-1][c1-1];
             }
         }
         return ans;
