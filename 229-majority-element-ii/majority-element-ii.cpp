8de1b2a0d5690c186d5f0784dcd0958309a83ca2
aadibansal801 <aadibansal801@gmail.com>
2026-07-31 14:54:04 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (46.43%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,21 +1,34 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        int l = 0;
         int n = nums.size();
+        int candid1,candid2;
+        int vote1=0,vote2=0;
         vector<int>ans;
-        while(l<n){
-            int r = l;
-            while(r<n && nums[r] == nums[l]){
-                r++;
+        for(int i = 0; i<n; i++){
+            if(candid1 == nums[i]){
+                vote1++;
+            }else if(candid2 == nums[i]){
+                vote2++;
+            }else if(vote1 == 0){
+                candid1 = nums[i];
+                vote1 = 1;
+            }else if(vote2 == 0){
+                candid2 = nums[i];
+                vote2 = 1;
+            }else{
+                vote1--;
+                vote2--;
             }
-            int count = r-l;
-            if(count>n/3){
-                ans.push_back(nums[l]);
-            }
-            l = r;
         }
+        int cnt1 = 0;
+        int cnt2 = 0;
+        for(auto it: nums){
+            if(it == candid1) cnt1++;
+            if(it == candid2) cnt2++;
+        }
+        if(cnt1>n/3) ans.push_back(candid1);
+        if(cnt2>n/3) ans.push_back(candid2);
         return ans;
     }
 };
