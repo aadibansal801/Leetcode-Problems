30a0de0e37ed096747fce58dbb0eacf9ff836be3
aadibansal801 <aadibansal801@gmail.com>
2026-08-03 01:02:05 +0530

    Time: 3 ms (7.34%) | Memory: 10.4 MB (73.76%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int k) {
+        if(k>nums.size()) return 0;
+        int low = *max_element(nums.begin(), nums.end());
+        int high = accumulate(nums.begin(), nums.end(), 0);
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if(isValid(nums,k,mid)){
+                high = mid-1;
+            }else{ 
+                low = mid+1;
+            }
+        }
+        return low;
+    }
+    bool isValid(vector<int>&nums, int k, int mid){ 
+        int sum = 0;
+        int count = 1;
+        for(auto it: nums){
+            if(sum + it > mid){
+                count++;
+                sum = it;
+            }else{
+                sum+=it;
+            }
+        }
+        return count<=k;
+    }
+};
