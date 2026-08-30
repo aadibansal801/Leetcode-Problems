29458b7cd27a4f500422bd120b25a5ed44770790
aadibansal801 <aadibansal801@gmail.com>
2026-08-30 21:57:48 +0530

    Time: 290 ms (99.49%) | Memory: 34 MB (93.77%) - LeetSync

--- 4380-minimum-operations-to-form-subset-sum-ii/minimum-operations-to-form-subset-sum-ii.cpp
+++ 4380-minimum-operations-to-form-subset-sum-ii/minimum-operations-to-form-subset-sum-ii.cpp
@@ -1,53 +1,49 @@
 class Solution {
 public:
 
-    int solve(int i, int sum, vector<int>&nums, vector<vector<int>>&dp){
-        if(i == nums.size()){
-            if(sum == 0){
-                return 0;
-            }else return 10000;
-        }
-        if(dp[i][sum]!=INT_MAX) return dp[i][sum];
-        int op = 0, bb = nums[i];
-        int ans = solve(i+1,sum,nums,dp);
-        if(nums[i]<=sum){
-            ans = min(ans, solve(i+1,sum - nums[i], nums, dp));
-        }
-        while(bb>0){
-            bb/=2; op++;
-            int cc = bb;
-            if(cc == 0){
-                break;
-            }
-            int op1 = op;
-            while(cc<=sum){
-                cc*=2; op1++;
-                if(cc<=sum){
-                    ans = min(ans, (op1 + solve(i+1, sum-cc, nums, dp)));
-                }
-            }
-            if(bb<=sum){
-                ans = min(ans, (op + solve(i+1,sum-bb,nums,dp)));
-            }
-        }
-        bb = nums[i], op = 0;
-        while(bb<=sum){
-            bb *= 2; op++;
-            if(bb<=sum){
-                ans = min(ans, (op + solve(i+1, sum - bb, nums, dp)));
-            }
-        }
-        return dp[i][sum] = ans;
-    }
-
     int minOperations(vector<int>& nums, int sum) {
         int n = nums.size();
-        int sumx = sum * 2;
-        vector<vector<int>>dp(n,vector<int>(sumx+10, INT_MAX));
-        int ans = solve(0,sum,nums,dp);
-        if(ans >= 10000){
-            return -1;
+        const int INF = 10000;
+        vector<int>curr(sum+1,INF);
+        vector<int>next(sum+1,INF);
+        next[0] = 0;
+        for(int i = n-1; i>=0; i--){
+            for(int s = 0; s<=sum; s++){
+                int ans = next[s];
+                if(nums[i]<=s){
+                    ans = min(ans, next[s-nums[i]]);
+                }
+                int bb = nums[i];
+                int op = 0;
+                while(bb>0){
+                    bb/=2; op++;
+                    int cc = bb;
+                    if(cc == 0){
+                        break;
+                    }
+                    int op1 = op;
+                    while(cc <= s){
+                        cc*=2, op1++;
+                        if(cc<=s){
+                            ans = min(ans, op1 + next[s-cc]);
+                        }
+                    }
+                    if(bb<=s){
+                        ans = min(ans, op + next[s-bb]);
+                    }
+                }
+                bb = nums[i];
+                op = 0;
+                while(bb<=s){
+                    bb*=2; op++;
+                    if(bb<=s){
+                        ans = min(ans, op + next[s-bb]);
+                    }
+                }
+                curr[s] = ans;
+            }
+            next = curr;
         }
-        return ans;
+        return next[sum] >= INF ? -1 : next[sum];
     }
 };
