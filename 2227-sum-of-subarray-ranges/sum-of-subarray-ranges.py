7e890d3b5f66dbf60f7ca62c4416afe6765e3a84
aadibansal801 <aadibansal801@gmail.com>
2026-09-04 14:23:43 +0530

    Time: 19 ms (72.77%) | Memory: 19.4 MB (52.11%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
@@ -1,12 +1,73 @@
 class Solution:
     def subArrayRanges(self, nums: List[int]) -> int:
-        ans = 0
-        n = len(nums)
-        for i in range(0,n):
-            largest = nums[i]
-            smallest = nums[i]
-            for j in range(i+1,n):
-                largest = max(largest,nums[j])
-                smallest = min(smallest, nums[j])
-                ans+=largest-smallest
-        return ans
+        def sumSubarrayMaxs(nums):
+            def findNGE(nums):
+                st = []
+                n = len(nums)
+                ans = [0] * n
+                for i in range(n-1,-1,-1):
+                    while st and nums[st[-1]]<=nums[i]:
+                        st.pop()
+                    ans[i] = st[-1] if st else n
+                    st.append(i)
+                return ans
+            
+            def findPGE(nums):
+                st = []
+                n = len(nums)
+                ans = [0] * n
+                for i in range(0,n):
+                    while st and nums[st[-1]]<nums[i]:
+                        st.pop()
+                    ans[i] = st[-1] if st else -1
+                    st.append(i)
+                return ans
+            
+            nge = findNGE(nums)
+            pge = findPGE(nums)
+            n = len(nums)
+            total = 0
+            for i in range(0,n):
+                left = i - pge[i]
+                right = nge[i] - i
+                freq = left * right
+                val = freq * nums[i]
+                total+=val
+            return total
+
+        def sumSubarrayMins(nums):
+            def findNSE(nums):
+                st = []
+                n = len(nums)
+                ans = [0] * n
+                for i in range(n-1, -1, -1):
+                    while st and nums[st[-1]] >= nums[i]:
+                        st.pop()
+                    ans[i] = st[-1] if st else n
+                    st.append(i)
+                return ans
+            
+            def findPSE(nums):
+                st = []
+                n = len(nums)
+                ans = [0]*n
+                for i in range(0,n):
+                    while st and nums[st[-1]] > nums[i]:
+                        st.pop()
+                    ans[i] = st[-1] if st else -1
+                    st.append(i)
+                return ans
+            
+            nse = findNSE(nums)
+            pse = findPSE(nums)
+            n = len(nums)
+            total = 0
+            for i in range(0,n):
+                left = i - pse[i]
+                right = nse[i] - i
+                freq = left * right
+                val = freq * nums[i]
+                total += val
+            return total
+
+        return sumSubarrayMaxs(nums) - sumSubarrayMins(nums)
