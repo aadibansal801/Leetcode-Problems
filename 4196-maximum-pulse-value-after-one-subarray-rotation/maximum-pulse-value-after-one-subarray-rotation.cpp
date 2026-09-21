56acf4938c682619d56a693f3b8567b21b70c070
aadibansal801 <aadibansal801@gmail.com>
2026-09-22 01:53:44 +0530

    Time: 12 ms (32.54%) | Memory: 155.7 MB (32.00%) - LeetSync

---
+++ 4196-maximum-pulse-value-after-one-subarray-rotation/maximum-pulse-value-after-one-subarray-rotation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    long long maxValue(vector<int>& nums) {
+        int n = nums.size();
+        vector<long long>prefix(n+1,0);
+        for(int i = 0; i<n; i++){
+            int val = (i%2==0)?nums[i]:-nums[i];
+            prefix[i+1] = prefix[i]+val;
+        }
+        long long original = prefix[n];
+        long long maxPrefix[2] = {
+            prefix[0],
+            LLONG_MIN
+        };
+        long long bestDelta = 0;
+        for(int r = 1; r<n; r++){
+            int parity = (r+1)%2;
+            if(maxPrefix[parity] != LLONG_MIN){
+                long long delta = 2 * (maxPrefix[parity] - prefix[r+1]);
+                bestDelta = max(delta, bestDelta);
+            }
+            int p = r%2;
+            maxPrefix[p]  = max(maxPrefix[p], prefix[r]);
+        }
+        return original + bestDelta;
+    }
+};
